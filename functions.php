47a76bb6c7a700dbc92e208f7b744fe9879b1ed8
danialbleile <danial.bleile@wsu.edu>
2014-12-15 09:14:45 -0800

    Added Frontpage Center Slideshow and .gitignore

--- functions.php
+++ functions.php
@@ -8,6 +8,8 @@ class Init_CAHNRSWP_Commodities {
 		
 		add_action( 'wp_enqueue_scripts', array( $this, 'cahnrswp_wp_enqueue_scripts' ), 20 );
 		
+		add_action( 'widgets_init' , array( $this, 'cahnrswp_widgets_init' ) );
+		
 	}
 	
 	public function cahnrswp_wp_enqueue_scripts() {
@@ -15,6 +17,22 @@ class Init_CAHNRSWP_Commodities {
 		wp_enqueue_script( 'cycle2', CAHNRSCOMMODITIESURL . '/js/cycle2.js' , array(), '2.1.6', false );
 		
 	}
+	
+	public function cahnrswp_widgets_init(){
+		
+		$args = array(
+			'name'          => __( 'Front Page Center', 'theme_text_domain' ),
+			'id'            => 'cwp-frontpage-center',
+			'description'   => '',
+        	'class'         => '',
+			'before_widget' => '',
+			'after_widget'  => '',
+			'before_title'  => '<h2 class="widgettitle">',
+			'after_title'   => '</h2>' );
+			
+		register_sidebar( $args );
+		
+	} // end cahnrswp_widgets_init
 	
 	/**
 	 * @desc Gets the horizontal navigation menu for the site
--- parts/featured-images.php
+++ parts/featured-images.php
@@ -1,36 +1,14 @@
 <?php if ( is_front_page() ):?>
 <div id="site-home-section">
 	<div class="site-home-column">
-    	<h1>Tree Fruit</h1>
-        <h2>Washington State University Comprehensive Tree Fruit Site</h2>
+    	<h1><?php the_title();?></h1>
+        <h2><?php bloginfo( 'description' ); ?> </h2>
         <div class="site-home-widgets">
-        	
+        	<?php if ( is_active_sidebar( 'cwp-frontpage-center' ) ) {
+                dynamic_sidebar( 'cwp-frontpage-center' );
+			}; ?>
         </div>
     </div>
 </div>
-<div id="cahnrs-treefruit-footer">
-	<nav class="fruit-nav">
-        <a class="fruit-option apple" href="http://treefruitdev.topics.wpdev.cahnrs.wsu.edu/apple-resources/">
-            <span class="icon">
-                <img src="http://treefruit.topics.wpdev.cahnrs.wsu.edu/wp-content/themes/treefruit/images/apple.png" />
-            </span>
-            <span class="title">Apple Resources</span>
-        </a><a class="fruit-option apple" href="http://treefruitdev.topics.wpdev.cahnrs.wsu.edu/apple-resources/">
-            <span class="icon">
-                <img src="http://treefruit.topics.wpdev.cahnrs.wsu.edu/wp-content/themes/treefruit/images/pear.png" />
-            </span>
-            <span class="title">Pear Resources</span>
-        </a><a class="fruit-option apple" href="#">
-            <span class="icon">
-                <img src="http://treefruit.topics.wpdev.cahnrs.wsu.edu/wp-content/themes/treefruit/images/cherry.png" />
-            </span>
-            <span class="title">Cherry Resources</span>
-        </a><a class="fruit-option apple" href="#">
-            <span class="icon">
-                <img src="http://treefruit.topics.wpdev.cahnrs.wsu.edu/wp-content/themes/treefruit/images/peach.png" />
-            </span>
-            <span class="title">Stone Fruit Resources</span> 
-        </a>
-    </nav>
-</div>
+<?php get_template_part( 'inc/inc-treefruit-frontpage-footer' ) ;?>
 <?php endif;?>
