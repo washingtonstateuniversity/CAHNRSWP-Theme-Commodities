88968078fc16389d7b8b09c28b30a81f3f257aa1
danialbleile <danial.bleile@wsu.edu>
2014-12-16 06:14:23 -0800

    Responsive Updates / Added Sidebar

---
+++ sidebar.php
@@ -0,0 +1,5 @@
+<div id="cahnrs-sidebar">
+    <a href="#" class="sidebar-title-action"><?php echo apply_filters( 'cahnrswp_sidebar_title' , 'Tools' );?></a>
+    <aside class="sidebar-widget-area">
+    </aside>
+</div>
