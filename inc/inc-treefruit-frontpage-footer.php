5bd10b9caff92a548968510152cbc38d9fc01853
danialbleile <danial.bleile@wsu.edu>
2015-02-03 06:18:15 -0800

    Closes Issue #1 2.3, 8.4

--- inc/inc-treefruit-frontpage-footer.php
+++ inc/inc-treefruit-frontpage-footer.php
@@ -4,15 +4,15 @@
             <span class="icon">
                 <img src="http://treefruit.topics.wpdev.cahnrs.wsu.edu/wp-content/themes/treefruit/images/apple.png" />
             </span>
-        </a><a class="fruit-option apple" href="http://treefruitdev.topics.wpdev.cahnrs.wsu.edu/apple-resources/">
+        </a><a class="fruit-option pear" href="http://treefruitdev.topics.wpdev.cahnrs.wsu.edu/apple-resources/">
             <span class="icon">
                 <img src="http://treefruit.topics.wpdev.cahnrs.wsu.edu/wp-content/themes/treefruit/images/pear.png" />
             </span>
-        </a><a class="fruit-option apple" href="#">
+        </a><a class="fruit-option cherry" href="#">
             <span class="icon">
                 <img src="http://treefruit.topics.wpdev.cahnrs.wsu.edu/wp-content/themes/treefruit/images/cherry.png" />
             </span>
-        </a><a class="fruit-option apple" href="#">
+        </a><a class="fruit-option stone-fruit" href="#">
             <span class="icon">
                 <img src="http://treefruit.topics.wpdev.cahnrs.wsu.edu/wp-content/themes/treefruit/images/peach.png" />
             </span> 
