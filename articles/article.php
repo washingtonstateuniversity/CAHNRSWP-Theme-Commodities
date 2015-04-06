2fa5604f6d2d2f4afc4c97b52c0482f60f21c920
danialbleile <danial.bleile@wsu.edu>
2015-04-06 08:36:56 -0700

    Version 0.0.12

--- articles/article.php
+++ articles/article.php
@@ -8,7 +8,7 @@
             	<a href="#" class="apple-section-icon section-icon" data-class="apple-2"></a>
                 <a href="#" class="pear-section-icon section-icon" data-class="pear-2"></a>
                 <a href="#" class="cherry-section-icon section-icon" data-class="cherry-2"></a>
-                <a href="#" class="peach-section-icon section-icon" data-class="stone-fruit-2"></a>
+                <a href="#" class="peach-section-icon section-icon" data-class="stone-fruit-section"></a>
                 <script>
 				!function(c,t){t.tf_icons=function(){jQuery(c).ready(function(){jQuery(".section-icon").each(function(){var c=jQuery(this),t=c.data("class"),n=jQuery("."+t).first();n.length>0&&(c.addClass("active"),c.on("click",function(c){jQuery("html").animate({scrollTop:n.offset().top},500,function(){n.hasClass("ccl-article-accordion")&&(c.preventDefault(),n.children(".ccl-title").trigger("click"))}),jQuery("body").animate({scrollTop:n.offset().top},500)}))})})};new tf_icons}(document,window);
 				</script>
