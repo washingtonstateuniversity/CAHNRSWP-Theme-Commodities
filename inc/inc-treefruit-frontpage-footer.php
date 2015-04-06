33332342c3c3d3a78ba51128a5d36aeaa6b3445a
danialbleile <danial.bleile@wsu.edu>
2015-04-06 08:53:36 -0700

    Version 0.0.13

--- inc/inc-treefruit-frontpage-footer.php
+++ inc/inc-treefruit-frontpage-footer.php
@@ -4,17 +4,29 @@
             <span class="icon">
                 <img src="http://treefruit.topics.wpdev.cahnrs.wsu.edu/wp-content/themes/treefruit/images/apple.png" />
             </span>
+            <span class="text">
+            	Apple Resources
+            </span>
         </a><a class="fruit-option pear" href="http://treefruitdev.topics.wpdev.cahnrs.wsu.edu/category/pear-2">
             <span class="icon">
                 <img src="http://treefruit.topics.wpdev.cahnrs.wsu.edu/wp-content/themes/treefruit/images/pear.png" />
+            </span>
+            <span class="text">
+            	Pear Resources
             </span>
         </a><a class="fruit-option cherry" href="http://treefruitdev.topics.wpdev.cahnrs.wsu.edu/category/cherry-2">
             <span class="icon">
                 <img src="http://treefruit.topics.wpdev.cahnrs.wsu.edu/wp-content/themes/treefruit/images/cherry.png" />
             </span>
+            <span class="text">
+            	Cherry Resources
+            </span>
         </a><a class="fruit-option stone-fruit" href="http://treefruitdev.topics.wpdev.cahnrs.wsu.edu/category/stone-fruit-section">
             <span class="icon">
                 <img src="http://treefruit.topics.wpdev.cahnrs.wsu.edu/wp-content/themes/treefruit/images/peach.png" />
+            </span>
+            <span class="text">
+            	Stone Fruit Resources
             </span> 
         </a>
     </nav>
