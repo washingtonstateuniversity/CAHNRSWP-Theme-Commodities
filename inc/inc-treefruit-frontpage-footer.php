ea94aab1c5202ae30b7e1c137bbe585f10f0fec0
danialbleile <danial.bleile@wsu.edu>
2015-04-21 11:29:28 -0700

    Version 1.0.0

--- inc/inc-treefruit-frontpage-footer.php
+++ inc/inc-treefruit-frontpage-footer.php
@@ -1,29 +1,29 @@
 <div id="cahnrs-treefruit-footer">
 	<nav class="fruit-nav">
-        <a class="fruit-option apple" href="http://treefruitdev.topics.wpdev.cahnrs.wsu.edu/category/apple-2">
+        <a class="fruit-option apple" href="/category/apple-2">
             <span class="icon">
-                <img src="http://treefruit.topics.wpdev.cahnrs.wsu.edu/wp-content/themes/treefruit/images/apple.png" />
+                <img src="<?php echo CAHNRSCOMMODITIESURL ;?>/images/apple.png" />
             </span>
             <span class="text">
             	Apple Resources
             </span>
-        </a><a class="fruit-option pear" href="http://treefruitdev.topics.wpdev.cahnrs.wsu.edu/category/pear-2">
+        </a><a class="fruit-option pear" href="/category/pear-2">
             <span class="icon">
-                <img src="http://treefruit.topics.wpdev.cahnrs.wsu.edu/wp-content/themes/treefruit/images/pear.png" />
+                <img src="<?php echo CAHNRSCOMMODITIESURL ;?>/images/pear.png" />
             </span>
             <span class="text">
             	Pear Resources
             </span>
-        </a><a class="fruit-option cherry" href="http://treefruitdev.topics.wpdev.cahnrs.wsu.edu/category/cherry-2">
+        </a><a class="fruit-option cherry" href="/category/cherry-2">
             <span class="icon">
-                <img src="http://treefruit.topics.wpdev.cahnrs.wsu.edu/wp-content/themes/treefruit/images/cherry.png" />
+                <img src="<?php echo CAHNRSCOMMODITIESURL ;?>/images/cherry.png" />
             </span>
             <span class="text">
             	Cherry Resources
             </span>
-        </a><a class="fruit-option stone-fruit" href="http://treefruitdev.topics.wpdev.cahnrs.wsu.edu/category/stone-fruit-section">
+        </a><a class="fruit-option stone-fruit" href="/category/stone-fruit-section">
             <span class="icon">
-                <img src="http://treefruit.topics.wpdev.cahnrs.wsu.edu/wp-content/themes/treefruit/images/peach.png" />
+                <img src="<?php echo CAHNRSCOMMODITIESURL ;?>/images/peach.png" />
             </span>
             <span class="text">
             	Stone Fruit Resources
